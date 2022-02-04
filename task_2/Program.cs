8b897141d6fdefaafb01e7b3f3ddc65983912ea0
«Artemka» <sergeev_artem91@mail.ru>
2022-02-04 15:25:19 +0500

    2. Даны два числа. Показать большее и меньшее число

---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (a > b)
+{
+    Console.WriteLine("Большее число: " + a);
+    Console.WriteLine("Меньшее число: " + b);
+}
+else
+{
+    Console.WriteLine("Большее число: " + b);
+    Console.WriteLine("Меньшее число: " + a);
+}
