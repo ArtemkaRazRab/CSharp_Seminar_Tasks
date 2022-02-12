6d05be47cc54af6f0e94f5c7413d099904210640
«Artemka» <sergeev_artem91@mail.ru>
2022-02-12 07:10:30 +0500

    27_2. Определить количество цифр в числе

---
+++ task_27_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 27_2. Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int digitCount(int a)
+{
+    int counter=0;
+    while(a>0)
+    {
+        counter++;
+        a=a/10;
+    }
+    return counter;
+}
+string str = Console.ReadLine();
+
+int x = Convert.ToInt32(str);
+
+Console.WriteLine("Количество цифр в числе " + digitCount(x));
+
