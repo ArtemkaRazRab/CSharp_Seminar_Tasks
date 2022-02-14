63bc9a924e7507ae1c91b3363a12aa16ff60fcc6
«Artemka» <sergeev_artem91@mail.ru>
2022-02-14 15:44:55 +0500

    9_2. Показать последнюю цифру трёхзначного числа

---
+++ task009_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 9_2. Показать последнюю цифру трёхзначного числа
+
+int Number(int b)
+{
+    int num = b % 10;
+    return num;
+}
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int c = Number(a);
+Console.WriteLine("Последняя цифра " + c);
