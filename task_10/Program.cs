79b16c33af43a81771567fa95bbafa028a098e2c
ArtemkaRazRab <98047006+ArtemkaRazRab@users.noreply.github.com>
2022-02-14 16:17:06 +0500

    Delete task_10 directory

--- task_10/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// 10. Показать вторую цифру трёхзначного числа
-
-int Number = new Random().Next(100, 999);
-int a = Number / 10;
-int b = a % 10;
-
-Console.WriteLine("Заданное число: " + Number);
-Console.WriteLine("Вторая цифра числа: " + b);
-
