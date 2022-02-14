a9c157e3d1c3d8122d0cebf62fa947ad5c8e2e55
ArtemkaRazRab <98047006+ArtemkaRazRab@users.noreply.github.com>
2022-02-14 16:12:30 +0500

    Delete task_31_1 directory

--- task_31_1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// 31_1. Задать массив из 8 элементов и вывести их на экран
-
-int[] mas = { 1, 2, 3, 4, 5, 6, 7, 8 };
-void function(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-}
-function(mas);
