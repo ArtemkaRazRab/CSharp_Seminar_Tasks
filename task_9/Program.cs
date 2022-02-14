6388016a326b8e8fb8f126241fe0fa70d40ea018
ArtemkaRazRab <98047006+ArtemkaRazRab@users.noreply.github.com>
2022-02-14 16:11:14 +0500

    Delete task_9 directory

--- task_9/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// 9. Показать последнюю цифру трёхзначного числа
-
-Console.WriteLine("Введите число ");
-string num = Console.ReadLine();
-int y = num[num.Length - 1] - '0';
-int n = y * y;
-
-Console.WriteLine("Последняя цифра " + y);
