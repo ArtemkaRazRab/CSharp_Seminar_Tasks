c2ec108fcc5859d347bf6e0d979d1dc7110823d7
«Artemka» <sergeev_artem91@mail.ru>
2022-02-22 17:20:00 +0500

    48. Показать двумерный массив размером m×n заполненный целыми числами

---
+++ task048/Program.cs
@@ -0,0 +1,63 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+void FillArray(int[,] array)                                          
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 5;
+int n = 4;
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+// void FillArray(double[,] array)                                          
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().NextDouble();
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int m = 5;
+// int n = 4;
+
+// double[,] array = new double [m, n];
+
+// FillArray(array);
+// PrintArray(array);
