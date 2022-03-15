fcda2daa15312c74b158b92e1ae082a792ef0b4c
«Artemka» <sergeev_artem91@mail.ru>
2022-03-15 16:08:10 +0500

    74. В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита

---
+++ task074/Program.cs
@@ -0,0 +1,29 @@
+﻿// 74. В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». 
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+string alphabet = "аисв";
+int n = 3;
+string[] Words(string a, int num)
+{
+    if (num == 0)
+    {
+        return new string[]{""};
+    }
+    string[] w = Words(a, num - 1);
+    string[] result = new string[w.Length * 4];
+    int k = 0;
+    for (int i = 0; i < w.Length; i++)
+    {
+        for (int j = 0; j < a.Length; j++)
+        {
+            result[k] = w[i] + a[j];
+            k++;
+        }
+    }
+    return result;
+}
+string[] res = Words(alphabet, n);
+for (int i = 0; i < res.Length; i++)
+{
+    Console.Write(res[i] + " ");
+}
